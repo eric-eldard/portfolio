3dba8175a0f31d310dd7ff1b735842b389c84b65
Eric Eldard <eric.eldard@gmail.com>
2024-11-22 20:49:30 -0500

    run script typo fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -18,7 +18,7 @@ printf "Fetching ${WHITE}database credentials${RESET}...\n"
 json=`aws secretsmanager get-secret-value --secret-id portfolio-app-rds-access --query SecretString --output text`
 host=`echo $json | jq -r ".host"`
 port=`echo $json | jq -r ".port"`
-db=`echo $json | jq -r ".dbInstanceIdentifier"`
+db=`echo $json | jq -r ".dbname"`
 export SPRING_DATASOURCE_URL="jdbc:mysql://${host}:${port}/${db}"
 export SPRING_DATASOURCE_USERNAME=`echo $json | jq -r ".username"`
 export SPRING_DATASOURCE_PASSWORD=`echo $json | jq -r ".password"`
