f05b8b8211374136fbe0de9f72ac03dd59d906ed
Eric Eldard <eric.eldard@gmail.com>
2024-12-20 23:25:56 -0500

    js minification

--- scripts/build-typescript.sh
+++ scripts/build-typescript.sh
@@ -1,3 +1,18 @@
+mkdir -p out/min
+
 # https://www.npmjs.com/package/typescript
 # https://www.npmjs.com/package/browserify
 node_modules/.bin/tsc && node_modules/.bin/browserify out/bundler.js -o out/portfolio.bundle.js
+
+# https://www.npmjs.com/package/uglify-js
+node_modules/.bin/uglifyjs out/portfolio.bundle.js \
+  --output out/min/portfolio.bundle.min.js \
+  --source-map "base=out,url=portfolio.bundle.min.js.map" \
+  --mangle \
+  --compress
+
+node_modules/.bin/uglifyjs out/utils.js \
+  --output out/min/utils.min.js \
+  --source-map "base=out,url=utils.min.js.map" \
+  --mangle \
+  --compress
