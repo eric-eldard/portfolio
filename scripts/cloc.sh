7a3bb354d06b0a0e322104b10562dfe17bf55cd8
Eric Eldard <eric.eldard@gmail.com>
2025-09-02 16:18:31 -0400

    fixes "cloc requires global install"

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -4,7 +4,7 @@ BOLD_WHITE="\e[1;97m"
 RESET="\e[0m"
 
 printf "\n\n${BOLD_WHITE}Lines of application code${RESET}\n\n"
-cloc --not-match-d="(node_modules|out|generated)" \
+npx cloc --not-match-d="(node_modules|out|generated)" \
  maintenance-page \
  scripts \
  sql \
@@ -12,7 +12,7 @@ cloc --not-match-d="(node_modules|out|generated)" \
  pom.xml
 
 printf "\n\n${BOLD_WHITE}Lines of application test code${RESET}\n\n"
-cloc --fullpath --not-match-d="resources/test/portfolio" src/test
+npx cloc --fullpath --not-match-d="resources/test/portfolio" src/test
 
 printf "\n\n${BOLD_WHITE}Lines of portfolio asset code${RESET}\n\n"
-cloc assets/portfolio
+npx cloc assets/portfolio
